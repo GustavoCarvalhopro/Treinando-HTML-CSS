ace12c313ca96baa4feeb85dd3579a0f9edd9446
GustavoCarvalhopro <gustavor.carvalho@hotmail.com>
2022-03-16 06:38:13 -0300

    second commit

---
+++ zoeira.js
@@ -0,0 +1,10 @@
+function Converter(){
+	var ValorElemento = document.getElementById("nome");
+	var Valor = ValorElemento.value;
+	
+	var passNome = document.getElementById("NomeUsu");
+	var Nome = "Prazer em te conhecer " + Nome
+	passNome.innerHTML
+
+	
+}
